f6152c5822ab720e9541e3f4f50f113c6cf76a77
Siddharth Chaudhary <siddharth.93.chaudhary@gmail.com>
2021-04-09 11:48:18 +0530

    the game function works and the final results are satisfactory

--- README.md
+++ README.md
@@ -6,3 +6,18 @@ adding the required functions necessary to execute the commands
 
 made the playRound function and tested various inputs 
 also used toLowerCase to make in input case insensitive
+
+
+made some more edits to the base js playRound to make the output more suitable
+
+finished the game function and the final tweeks to make the game display the results in the console
+
+the logic works perfectly for:
+* The base function to generate the tandom computer output
+* THe playRound function to play a round between the comp and the user
+* the game function to play a game of 5 rounds and then decide who won in the end
+
+
+things to implement later:
+* UI
+* all the necessart html fields and buttons to actually play the game on a webpage
--- javascript.js
+++ javascript.js
@@ -4,15 +4,11 @@ const outcome = ['Rock','Paper','Scissors'];
 
 const computerPlay = () => {
   let randomNumber =Math.round( Math.random() * (3 - 1) + 1 ) ;
-  console.log( `Computer: ${outcome[ randomNumber - 1 ]}` );
+  // console.log( `Computer: ${outcome[ randomNumber - 1 ]}` );
   return outcome[ randomNumber - 1 ];
 }
 
-var playerSelection = prompt('Enter your selection');
-console.log(`Player: ${playerSelection}`);
 
-
-var computerSelection = computerPlay();
 
 
 const playRound = (playerSelection,computerSelection) => {
@@ -21,14 +17,14 @@ const playRound = (playerSelection,computerSelection) => {
       ( playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Rock') ||( playerSelection.toLowerCase() === 'paper' && computerSelection === 'Scissors')
     )
   {
-    return 'Computer Wins';
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
   }
   else if( 
       (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Rock' ) || 
       ( playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Paper') ||( playerSelection.toLowerCase() === 'rock' && computerSelection === 'Scissors')
    )
   {
-    return 'Player Wins';
+    return `You Win! ${playerSelection[0].toUpperCase() + playerSelection.substr(1)} beats ${computerSelection}`;
   }
   else{
     return `It's a Draw`;
@@ -36,4 +32,37 @@ const playRound = (playerSelection,computerSelection) => {
  
 }
 
-console.log(playRound(playerSelection,computerSelection))
+
+
+
+const game = () =>{
+  let userCount=0;
+  let compCount=0
+  for (let i=0 ; i<5 ; i++){
+    var playerSelection = prompt('Enter your selection');
+    console.log(`Player: ${playerSelection}`);
+
+    var computerSelection = computerPlay();
+    console.log(`Computer: ${computerSelection}`);
+
+    var result = playRound(playerSelection,computerSelection);
+    console.log(result);
+    // console.log(`result : ${result.substr(4,1)}`);
+    if( result.substr(4,1) === 'W'){
+      userCount++;
+    }
+    else if( result.substr(4,1) === 'L'){
+      compCount++;
+    }
+    
+  }
+  if(userCount>=compCount){
+    return `User wins the game`;
+  }
+  else {
+    return `Computer wins the game`;
+  }
+  
+}
+
+console.log(game());
