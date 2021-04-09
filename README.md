852a571d9987e6d899db226a5f14e0a81d4601e6
Siddharth Chaudhary <siddharth.93.chaudhary@gmail.com>
2021-04-09 11:07:30 +0530

    finished the function playRound everything working good

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # TOP-RockPaperScissors
 
-created the index.html styles.css and the javascript.js files 
+created the index.html styles.css and the javascript.js files
+
+adding the required functions necessary to execute the commands
+
+made the playRound function and tested various inputs 
+also used toLowerCase to make in input case insensitive
--- javascript.js
+++ javascript.js
@@ -1,14 +1,39 @@
 // console.log('Hello World!')
 
-const outcome = ['Rock','Paper','Scissor'];
+const outcome = ['Rock','Paper','Scissors'];
 
 const computerPlay = () => {
   let randomNumber =Math.round( Math.random() * (3 - 1) + 1 ) ;
-
-  console.log( outcome[ randomNumber - 1 ] )
+  console.log( `Computer: ${outcome[ randomNumber - 1 ]}` );
+  return outcome[ randomNumber - 1 ];
 }
 
-var userSelection = prompt('Enter your selection')
+var playerSelection = prompt('Enter your selection');
+console.log(`Player: ${playerSelection}`);
+
 
 var computerSelection = computerPlay();
-computerPlay();
+
+
+const playRound = (playerSelection,computerSelection) => {
+  if( 
+      (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Paper' ) || 
+      ( playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Rock') ||( playerSelection.toLowerCase() === 'paper' && computerSelection === 'Scissors')
+    )
+  {
+    return 'Computer Wins';
+  }
+  else if( 
+      (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Rock' ) || 
+      ( playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Paper') ||( playerSelection.toLowerCase() === 'rock' && computerSelection === 'Scissors')
+   )
+  {
+    return 'Player Wins';
+  }
+  else{
+    return `It's a Draw`;
+  }
+ 
+}
+
+console.log(playRound(playerSelection,computerSelection))
